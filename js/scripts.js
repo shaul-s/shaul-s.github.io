9084ecb69353334167587f5a4cd4d665ffa2a199
shaul-s <58389445+shaul-s@users.noreply.github.com>
2021-07-27 11:15:05 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ console.log(width, height);
 var my_image = document.getElementById("myImage");
 // var my_name = document.getElementById("myName");
 
-if (width < 450){
+if (width < 500){
     my_image.style.display = "block";
     my_image.style.width = "50px";
     // my_name.remove()
